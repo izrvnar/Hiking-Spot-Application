1aa060b99e90f29c8889496a9d3cddb3692aeb43
Ivan Zrvnar <izrvnar@gmail,com>
2022-04-18 19:52:30 -0400

    Initial Commit

---
+++ IvanZrvnarHikingSpots/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IvanZrvnarHikingSpots
+//
+//  Created by Ivan Zrvnar on 2022-04-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
