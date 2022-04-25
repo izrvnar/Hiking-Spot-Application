136dd537eaf3187c6655d30e0ddea8fed41c6735
Ivan Zrvnar <izrvnar@gmail,com>
2022-04-24 21:00:46 -0400

    created the functionality for the user to double tap to open the camera and made the starting map their current location

--- IvanZrvnarHikingSpots/Controllers/DetailViewController.swift
+++ IvanZrvnarHikingSpots/Controllers/DetailViewController.swift
@@ -6,24 +6,70 @@
 //
 
 import UIKit
+import MapKit
 
-class DetailViewController: UIViewController {
-
+class DetailViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    //MARK: - Outlets
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var cameraImage: UIImageView!
+    
+    
+    //MARK: - Properties
+    var locationManager = CLLocationManager()
+    var currentLocation: CLLocation!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // loading the current location of the user
+        locationManager.delegate = self
+        mapView.delegate = self
+        
+        cameraImage.isUserInteractionEnabled = true
+  
+        
+        // using the satilite
+        mapView.showsUserLocation = true
+        mapView.mapType = .satellite
+        
+        guard CLLocationManager.locationServicesEnabled() else { return }
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        
+        if locationManager.authorizationStatus == .notDetermined{
+            locationManager.requestWhenInUseAuthorization()
+        }else {
+            locationManager.stopUpdatingLocation()
+        }
+        
+        // creating double tap gesture to open the camera
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(cameraDoubleTapped))
+        doubleTap.numberOfTapsRequired = 2
+        cameraImage.addGestureRecognizer(doubleTap)
+        
+    
+        
+    }//: View did load
+    
+    //MARK: -Methods
+    // getting acsess to the camera on the double tap 
+    @objc func cameraDoubleTapped() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
     }
+        
+
+}//: View Controller
+
+//MARK: - Extensions
+
+
+
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
-}
+    
